41a48eb7e19c7438d8203f62b6048204eb61302f
Guido De Rosa <job@guidoderosa.net>
2010-03-01 12:09:09 +0000

    use HAML templating

--- lib/hotspotlogin/app.rb
+++ lib/hotspotlogin/app.rb
@@ -1,11 +1,18 @@
 require 'rubygems'
 require 'sinatra/base'
+require 'haml'
 
 module HotSpotLogin 
   class App < Sinatra::Base
 
+    set :root, File.dirname(__FILE__) + '/../..'
+
+    not_found do
+      haml :"404"
+    end
+
     get '/' do
-      "hello!"
+      haml :hotspotlogin
     end
 
   end
