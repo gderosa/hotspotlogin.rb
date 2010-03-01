4870f34338f4b5c42ffa4a8b1e92ae338dfb0a55
Guido De Rosa <job@guidoderosa.net>
2010-03-01 17:53:18 +0000

    start "translating" from PHP

--- lib/hotspotlogin/app.rb
+++ lib/hotspotlogin/app.rb
@@ -55,6 +55,29 @@ module HotSpotLogin
 
     # Matches '/', '/hotspotlogin' and '/hotspotlogin.rb'
     get %r{^/(hotspotlogin(\.rb)?/?)?$} do 
+
+      if HotSpotLogin.config[:uamsecret] and
+          HotSpotLogin.config[:uamsecret].length > 0
+        uamsecret = HotSpotLogin.config[:uamsecret]
+      else
+        uamsecret = nil
+      end
+
+      # attempt to login
+      if params['login'] == 'login'
+        hexchal = params['chal'].chars.to_a.pack('H32')
+        if uamsecret
+          newchal = 
+              Digest::MD5.hexdigest(hexchal + uamsecret).chars.to_a.pack('H*')
+        else
+          newchal = hexchal
+        end
+        response = Digest::MD5.hexdigest("\0" + params['Password'] + newchal)
+        newpwd = params['Password'].chars.to_a.pack('a32') 
+        (newpwd ^ newchal).unpack('H32').join
+
+      end
+
       haml(
         :hotspotlogin,
         :locals => {
