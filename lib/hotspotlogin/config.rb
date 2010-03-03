76d46e4cf58465009b42c4021cd4f567111fd17e
Guido De Rosa <job@guidoderosa.net>
2010-03-03 12:00:27 +0000

    basic command line parsing

--- lib/hotspotlogin/config.rb
+++ lib/hotspotlogin/config.rb
@@ -1,13 +1,12 @@
+require 'optparse'
+
 module HotSpotLogin
-  # Identifiers are strongly modeled on ChilliSpot's hotspotlogin.cgi
-  # and hotspologin.php 
-  # (found in: http://www.chillispot.info/chilliforum/viewtopic.php?pid=80#p80)
+
+  # defaults
 
   UAMSECRET       = 'uamsecret'
   USERPASSWORD    = true
   PORT            = 4990
-
-  # TODO: do not hardcode, use cmdline options or config files 
 
   @@config        = {
     'uamsecret'     => UAMSECRET,
@@ -17,4 +16,29 @@ module HotSpotLogin
 
   def self.config; @@config; end
 
+  def self.config=(h); @@config = h; end
+
+  def self.parse_cmdline!
+    OptionParser.new do |opts|
+      opts.banner = "Usage: #{$0} [options]"
+
+      opts.on('--conf FILE', 'configuration file') do |filename|
+        @@config['conf'] = filename
+      end
+
+      opts.on('--uamsecret PASS', 'as in chilli.conf(5)') do |uamsecret|
+        @@config['uamsecret'] = uamsecret
+      end
+
+      opts.on('--userpassword', 'like setting $userpassword=1 in hotspotlogin.cgi') do |v|
+        @@config['userpassword'] = true
+      end
+
+      opts.on('--port PORT', 'TCP port to listen on') do |v|
+        @@config['port'] = v.to_i
+      end
+    end.parse!
+    pp @@config
+  end
+    
 end
