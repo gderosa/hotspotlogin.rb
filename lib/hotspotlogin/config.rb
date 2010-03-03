e7e7d2c11c638dece33f7a31942c5b49e0f1f2f1
Guido De Rosa <job@guidoderosa.net>
2010-03-03 12:37:35 +0000

    --listen-address

--- lib/hotspotlogin/config.rb
+++ lib/hotspotlogin/config.rb
@@ -7,12 +7,14 @@ module HotSpotLogin
 
   # UAMSECRET       = 'uamsecret'
   # USERPASSWORD    = true
+  LISTEN_ADDRESS  = '0.0.0.0'
   PORT            = 4990
 
   @@config        = {
     # 'uamsecret'     => UAMSECRET,
     # 'userpassword'  => USERPASSWORD,
-    'port'          => PORT
+    'listen-address'  => LISTEN_ADDRESS,
+    'port'            => PORT
   } 
 
   def self.config; @@config; end
@@ -33,13 +35,18 @@ module HotSpotLogin
         @@config['uamsecret'] = uamsecret
       end
 
-      opts.on('--userpassword', 'like setting $userpassword=1 in hotspotlogin.cgi') do |v|
+      opts.on('--userpassword', 'like setting $userpassword=1 in hotspotlogin.cgi') do |userpassword|
         @@config['userpassword'] = true
       end
 
-      opts.on('--port PORT', 'TCP port to listen on') do |v|
-        @@config['port'] = v.to_i
+      opts.on('--port PORT', 'TCP port to listen on') do |port|
+        @@config['port'] = port.to_i
       end
+
+      opts.on('--listen-address ADDR', 'IP address or hostname to listen on') do |addr|
+        @@config['listen-address'] = addr
+      end
+
     end.parse!
   end
     
