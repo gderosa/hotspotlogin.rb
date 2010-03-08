845a99efe1d9b344e939f2b15a9a5b45a1fd82c4
Guido De Rosa <job@guidoderosa.net>
2010-03-08 19:03:50 +0000

    [misc]

--- bin/hotspotlogin.rb
+++ bin/hotspotlogin.rb
@@ -16,26 +16,35 @@ require File.expand_path(
   File.dirname(file) + "/../lib/hotspotlogin"
 )
 
-config = HotSpotLogin.config!
+# The $0 hack also would break the following code :-/
 
-if config['daemon']
-  pid = fork do
-    if config['log'] 
-      STDOUT.reopen(config['log'], 'a')
-      STDERR.reopen(config['log'], 'a') 
-      Signal.trap('USR1') do
-        STDOUT.flush
-        STDERR.flush
+# RubyGems sucks 'cause it includes bin/ directories into the $LOAD_PATH
+#
+# So, this file may be actually used to just 'require' the 
+# hotspotlogin library (in this case $0 != __FILE__) 
+
+if $0 == __FILE__ # actually start hotspotlogin service
+  config = HotSpotLogin.config!
+
+  if config['daemon']
+    pid = fork do
+      if config['log'] 
+        STDOUT.reopen(config['log'], 'a')
+        STDERR.reopen(config['log'], 'a') 
+        Signal.trap('USR1') do
+          STDOUT.flush
+          STDERR.flush
+        end
+      end
+      HotSpotLogin::App.run!
+      FileUtils.rm config['pid'] if config['pid'] and File.exists? config['pid']
+    end
+    if config['pid'] =~ /\S/ 
+      File.open config['pid'], 'w' do |f|
+        f.write pid 
       end
     end
+  else # run in foreground
     HotSpotLogin::App.run!
-    FileUtils.rm config['pid'] if config['pid'] and File.exists? config['pid']
   end
-  if config['pid'] =~ /\S/ 
-    File.open config['pid'], 'w' do |f|
-      f.write pid 
-    end
-  end
-else
-  HotSpotLogin::App.run!
 end
