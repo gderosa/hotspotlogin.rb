881d9618e7651d35f7653e20b37cab77ba0636b5
Guido De Rosa <guido.derosa@vemarsas.it>
2010-11-11 17:46:41 +0000

    define numerical constants in HotSpotLogin::Result

--- lib/hotspotlogin/constants.rb
+++ lib/hotspotlogin/constants.rb
@@ -8,5 +8,18 @@ module HotSpotLogin
     'log-http'          => false,
     'userpassword'      => true # like $userpassword in hotpotlgin.(cgi|php)
   } 
+
+  # corresponding GET parameters are res=success, res=failed etc.
+  module Result
+    SUCCESS =  1
+    FAILED  =  2
+    LOGOFF  =  3  # logged out
+    ALREADY =  4
+    NOTYET  =  5
+    POPUP1  = 11
+    POPUP2  = 12
+    POPUP3  = 13
+    NONE    =  0  # It was not a form request
+  end
    
 end
